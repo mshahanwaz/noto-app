ba31e9e36a63e34099bc24d3f27c4a6a693793cb
M. Shahanwaz <mohdshahanwaz55@gmail.com>
2022-08-29 13:42:11 +0530

    fix: removed route push when unauthorized

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,11 +5,13 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="h-screen w-full text-gray-900 bg-gray-50 dark:bg-gray-900 dark:text-gray-50 flex flex-col justify-center items-center gap-4 overflow-hidden">
+    <div className="h-screen w-full bg-gray-50 dark:bg-gray-900 flex flex-col justify-center items-center gap-4 overflow-hidden">
       <Head>
         <title>Page Not Found</title>
       </Head>
-      <h1 className="text-[10rem] font-extrabold">404</h1>
+      <h1 className="text-[10rem] font-extrabold text-gray-600 dark:text-gray-300">
+        404
+      </h1>
       <Link href="/">
         <a className="px-4 py-2 bg-gray-900 text-gray-50 dark:bg-gray-50 dark:text-gray-900 font-medium rounded-lg inline-flex items-center gap-2">
           <ArrowLeftIcon />
